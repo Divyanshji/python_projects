df398b234e1d28f10f0e50b8ab679a855c915318
divyanshji <divyanshjainjpr@gmail.com>
2024-01-11 14:00:27 +0530

    few changes

---
+++ reverse_num.py
@@ -0,0 +1,12 @@
+import sys
+sys.setrecursionlimit(1500) #Updating the recursion limit to 1500. by Default is 1000 
+
+def countdown(i):
+    print(i)
+    if(i <=0 ):
+        return
+    else:
+        countdown (i- 1)
+
+num = int(input("enter a number to print till 0- "))
+result = countdown(num)
--- selectionSort.py
+++ selectionSort.py
@@ -19,17 +19,3 @@ print (selectionSort([5,1,55,12,9,33]))
 # list1 = [55,6,88,54,0,2]
 # list1.sort()
 # print (list1)
-
-# import sys
-# sys.setrecursionlimit(1500)
-# def countdown(i):
-#     print(i)
-#     if(i <=0 ):
-#         # print (i)
-#         return
-#     else:
-#         countdown (i- 1)
-#         # return
-
-# num = int(input("enter a number - "))
-# result = countdown(num)
